94d1f068d6a9cba4a9ac616bbbcf2cbe07b621d4
rooslucas <rooslucas@live.nl>
2020-04-21 20:41:13 +0200

    Add pig_latin project to notes of chapter 6

--- chapter-6/chapter_6.py
+++ chapter-6/chapter_6.py
@@ -76,3 +76,55 @@ printPicnic(picnicItems, 20, 6)
 pyperclip.copy('Hello, world!')
 print(pyperclip.paste())
 
+# English to Pig Latin
+print('Enter the English message to translate to Pig Latin *oink oink*:')
+message = input()
+pig_latin = [] # The list that wil contain Pig Latin words
+vowels = ('a', 'e', 'i', 'o', 'u', 'y')
+
+for word in message.split():
+    # Seperate the non-letters at the start of the word:
+    prefix_non_letters = ''
+    while len(word) > 0 and not word[0].isalpha():
+        prefix_non_letters += word[0]
+        word = word[1:]
+    if len(word) == 0:
+        pig_latin.append(prefix_non_letters)
+        continue
+
+    # Separate the non-letters at the end of this word:
+    suffix_non_letters = ''
+    while not word[-1].isalpha():
+        suffix_non_letters += word[-1]
+        word = word[:-1]
+
+    # Remember the casing of the words
+    was_upper = word.isupper()
+    was_title = word.istitle()
+
+    word = word.lower() # Useful for the translation
+
+    # Separate consonants at beginning of the word
+    prefix_consonants = ''
+    while len(word) > 0 and not word[0] in vowels:
+        prefix_consonants += word[0]
+        word = word[1:]
+
+    # Now where making the words pig Latin *oink oink*
+    if prefix_consonants != '':
+        word += prefix_consonants + 'ay'
+    else:
+        word += 'yay'
+
+    # Restore casing
+    if was_upper:
+        word = word.upper()
+    elif was_title:
+        word = word.title()
+
+    # Combine everthing as a word
+    pig_latin.append(prefix_non_letters + word + suffix_non_letters)
+
+# Add everything together as a big oinking family
+print(' '.join(pig_latin))
+
