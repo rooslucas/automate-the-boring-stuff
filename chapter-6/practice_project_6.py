6b89d423a496e8436597fb14e1f4c2ad2e7a05d8
rooslucas <rooslucas@live.nl>
2020-04-22 23:56:09 +0200

    Adjust variable names

--- chapter-6/practice_project_6.py
+++ chapter-6/practice_project_6.py
@@ -6,20 +6,20 @@ table_data = [['chocolate frogs', 'butter beer', 'jelly beans', 'pumpkin pasties
 
 def print_table(list):
     col_width = [0] * len(list)
-    location = 0
+    index = 0
 
     for inner_list in list:
-        col_width[location] = max(len(x) for x in inner_list)
+        col_width[index] = max(len(x) for x in inner_list)
         word_location = 0
         for word in inner_list:
-            list[location][word_location] = word.rjust(col_width[location])
+            list[index][word_location] = word.rjust(col_width[index])
             word_location += 1
-        location += 1
+        index += 1
 
-    for location_word in range(0, len(inner_list)):
+    for index_word in range(0, len(inner_list)):
         table = []
-        for location_list in range(0, len(list)):
-            table.append(list[location_list][location_word])
+        for index_list in range(0, len(list)):
+            table.append(list[index_list][index_word])
         print(' '.join(table))
 
 
