ce79149890ad87e7890dd355b99d632964a29b92
rooslucas <rooslucas@live.nl>
2020-04-23 20:32:54 +0200

    Add notes from chapter 7

---
+++ chapter_7/chapter_7.py
@@ -0,0 +1,33 @@
+# Notes on chapter 7
+# Pattern Matching with Regular Expressions
+
+# phone number check without regular expressions
+def is_phone_number(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+        if text[3] != '-':
+            return False
+        for i in range(4, 7):
+            if not text[i].isdecimal():
+                return False
+        if text[7] != '-':
+            return False
+        for i in range(8, 12):
+            if not text[i].isdecimal():
+                return False
+        return True
+
+# print('Is 415-555-4242 a phone number?')
+# print(is_phone_number('415-555-4242'))
+# print('Is Moshi moshi a phone number?')
+# print(is_phone_number('Moshi moshi'))
+
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if is_phone_number(chunk):
+        print('Phone number found: ' + chunk)
+print('Done')
