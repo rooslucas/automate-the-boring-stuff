880d3fa7633844133583a59ce7fdc5d51b3f3f11
rooslucas <rooslucas@live.nl>
2020-05-07 17:49:14 +0200

    Add practice project 2 of chapter 7

--- chapter-7/practice_project_7.py
+++ chapter-7/practice_project_7.py
@@ -27,7 +27,6 @@ short_months = ['04', '06', '09', '11']
 long_months = ['01', '03', '05', '07', '08', '10', '12']
 february = ['02']
 
-
 # Check if date is a valid date
 # Check the months with 30 days
 if month in short_months:
@@ -71,3 +70,31 @@ elif month in long_months:
         print(f'The date {my_date} is not a valid date.')
 else:
     print(f'The date {my_date} is not a valid date.')
+
+# Practice project 2: Strong Password Detection
+
+# Length regex
+length_regex = re.compile(r'.{8,}')
+
+# Digit regex
+digit_regex = re.compile(r'[0-9]')
+
+# Casing regex
+lower_casing_regex = re.compile(r'[a-z]')
+upper_casing_regex = re.compile(r'[A-Z]')
+
+print("Enter your password:")
+my_password = input()
+
+# Run through the regexes
+if (length_regex.search(my_password) is not None) & (digit_regex.search(my_password) is not None) & (
+        lower_casing_regex.search(my_password) is not None) & (upper_casing_regex.search(my_password) is not None):
+    print("You created a strong password.")
+elif length_regex.search(my_password) is None:
+    print("The minimum number of characters is eight, otherwise your password isn't great...")
+elif digit_regex.search(my_password) is None:
+    print("Babidiboo, you need a digit too...")
+elif (lower_casing_regex.search(my_password) is None) | (upper_casing_regex.search(my_password) is None):
+    print("You need lower and upper, otherwise you ain't getting supper...")
+else:
+    print("You failed so hard, please begin from the start...")
