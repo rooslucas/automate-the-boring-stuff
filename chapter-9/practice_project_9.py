6782124da12074b0872d3d6d7c8de10bba90419b
rooslucas <rooslucas@live.nl>
2020-06-24 23:24:54 +0200

    Fix punctuation placement

--- chapter-9/practice_project_9.py
+++ chapter-9/practice_project_9.py
@@ -6,40 +6,28 @@ import re
 # Open the file and create a list
 mad_lips_open = open(r'mad_lips_text.txt', 'r')
 mad_lips_text = mad_lips_open.read()
-list_mad_lips = mad_lips_text.split(' ')
-
-# Check for dots (also possible for other punctuation marks)
-dots = []
-for i in range(len(mad_lips_text)):
-    if mad_lips_text[i] == '.':
-        dots.append(i)
-mad_lips_text = mad_lips_text.replace('.', '')
 
 # Check the text for adjectives, nouns, adverbs and verbs
 words_regex = re.compile(r'ADJECTIVE|NOUN|ADVERB|VERB')
 for word in words_regex.findall(mad_lips_text):
     if word == 'ADJECTIVE':
         adjective = pyip.inputStr("Enter an adjective: ")
-        list_mad_lips[(list_mad_lips.index(word))] = adjective
+        mad_lips_text = mad_lips_text.replace('ADJECTIVE', adjective, 1)
+
     elif word == 'NOUN':
         noun = pyip.inputStr("Enter a noun: ")
-        mad_lips_text.replace('NOUN', noun)
-        list_mad_lips[(list_mad_lips.index(word))] = noun
+        mad_lips_text = mad_lips_text.replace('NOUN', noun, 1)
+
     elif word == 'ADVERB':
         adverb = pyip.inputStr("Enter an adverb: ")
-        list_mad_lips[(list_mad_lips.index(word))] = adverb
+        mad_lips_text = mad_lips_text.replace('ADVERB', adverb, 1)
+
     elif word == 'VERB':
         verb = pyip.inputStr("Enter a verb: ")
-        list_mad_lips[(list_mad_lips.index(word))] = verb
+        mad_lips_text = mad_lips_text.replace('VERB', verb, 1)
 
-# Combine the list into a string and add the punctuation marks
-str_mad_lips = ' '.join([str(word) for word in list_mad_lips])
-
-for i in range(len(dots)):
-    index = dots[i]
-    total_mad_lips = str_mad_lips[:index] + '.' + str_mad_lips[index:]
 
 # Print the new text and add to a new file
-print(total_mad_lips)
+print(mad_lips_text)
 new_mad_lips = open(r'new_mad_lips.txt', 'w+')
-new_mad_lips.write(total_mad_lips)
+new_mad_lips.write(mad_lips_text)
