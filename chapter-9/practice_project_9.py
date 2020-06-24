49c5fec690752269f5ea8b8ae445923bd3c3739a
rooslucas <rooslucas@live.nl>
2020-06-24 23:29:04 +0200

    Refactor word replacement

--- chapter-9/practice_project_9.py
+++ chapter-9/practice_project_9.py
@@ -10,22 +10,8 @@ mad_lips_text = mad_lips_open.read()
 # Check the text for adjectives, nouns, adverbs and verbs
 words_regex = re.compile(r'ADJECTIVE|NOUN|ADVERB|VERB')
 for word in words_regex.findall(mad_lips_text):
-    if word == 'ADJECTIVE':
-        adjective = pyip.inputStr("Enter an adjective: ")
-        mad_lips_text = mad_lips_text.replace('ADJECTIVE', adjective, 1)
-
-    elif word == 'NOUN':
-        noun = pyip.inputStr("Enter a noun: ")
-        mad_lips_text = mad_lips_text.replace('NOUN', noun, 1)
-
-    elif word == 'ADVERB':
-        adverb = pyip.inputStr("Enter an adverb: ")
-        mad_lips_text = mad_lips_text.replace('ADVERB', adverb, 1)
-
-    elif word == 'VERB':
-        verb = pyip.inputStr("Enter a verb: ")
-        mad_lips_text = mad_lips_text.replace('VERB', verb, 1)
-
+    new_word = pyip.inputStr(" Enter an " + word.lower() + ": ")
+    mad_lips_text = mad_lips_text.replace(word, new_word, 1)
 
 # Print the new text and add to a new file
 print(mad_lips_text)
