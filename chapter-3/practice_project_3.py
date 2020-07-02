4b0a506807ac7fe8f56f0be92f5a001f7cf38640
rooslucas <rooslucas@live.nl>
2020-07-02 15:54:33 +0200

    Add try and except statements

--- chapter-3/practice_project_3.py
+++ chapter-3/practice_project_3.py
@@ -1,22 +1,21 @@
 # Practice project from chapter 3
 
+
 # The Collatz Sequence
 def collatz(number):
-    even = number % 2 == 0
-    if even:
-        number = number // 2
-    elif not even:
-        number = 3 * number + 1
-    return number
+    if number % 2 == 0:
+        return number // 2
+    else:
+        return 3 * number + 1
 
 
-print(collatz(4))
+try:
+    print('Enter number: ')
+    number_input = int(input())
 
-print('Enter number: ')
-number = int(input())
-not_one = number != 1
+    while number_input != 1:
+        number_input = collatz(number_input)
+        print(number_input)
 
-while not_one:
-    number =  collatz(number)
-    print(number)
-    not_one = number != 1
+except ValueError:
+    print("You must enter an integer")
