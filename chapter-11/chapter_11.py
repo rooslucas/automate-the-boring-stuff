5ecf79c87c63a62d000f1689ea4678ceb6f2c345
rooslucas <rooslucas@live.nl>
2020-07-24 17:34:58 +0200

    Add notes and examples from chapter 11

--- chapter-11/chapter_11.py
+++ chapter-11/chapter_11.py
@@ -54,5 +54,4 @@ except:
 ages = [26, 57, 92, 54, 22, 15, 17, 80, 47, 73]
 ages.sort()
 ages = [15, 17, 22, 26, 47, 54, 57, 73, 80, 92]
-assert
-ages[0] <= ages[-1]  # Assert that the first age is <= the last age.
+assert ages[0] <= ages[-1]  # Assert that the first age is <= the last age.
